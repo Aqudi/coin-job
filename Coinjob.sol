a0e0b7e26cfeaea3113405fd9813ebb95c6a63e3
whduddhks <39693818+whduddhks@users.noreply.github.com>
2021-05-15 18:24:38 +0900

    job array return

--- Coinjob.sol
+++ Coinjob.sol
@@ -16,15 +16,23 @@ contract Coinjob {
     
     uint public postContract = 0.05 ether;
     
-    
-    
     function publishJob(string memory _title, string memory _content, uint _deadline, ){
         uint memory deadline = block.timestamp + _deadline; 
         Job memory newJob = Job(msg.sender, _title, _content, deadline, msg.value, 0, '', finish=0);
         job.push(newJob);
     }
     
+    function alljob() public returns (Job[] memory){
+        return job;
+    }
     
+    function getjob(uint _number) public returns(address writer, string memory title, string memory content, uint deadline, uint reward){
+        writer = job[_number].writer;
+        title = job[_number].title;
+        content = job[_number].content;
+        deadline = job[_number].deadline;
+        reward = job[_number].reward;
+    }
     
     function acceptJob(){
         
