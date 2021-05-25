fc5ef8174b6e88009282bf72841db3a7e963daef
Aqudi <gjdigj145@gmail.com>
2021-05-25 15:53:54 +0900

    Implements and test allJob, publishJob

---
+++ src/index.js
@@ -0,0 +1,142 @@
+(function () {
+    'use strict';
+}());
+
+import Web3 from "web3";
+import contractJson from "../build/contracts/Coinjob.json";
+import $ from "jquery";
+import contract from "@truffle/contract";
+
+window.jQuery = $;
+window.$ = $;
+
+const App = {
+    web3Provider: null,
+    contracts: {},
+    account: '0x0',
+
+    init: function () {
+        console.log("Init app")
+        return App.initWeb3();
+    },
+
+    initWeb3: function () {
+        console.log("Function initWeb3")
+        if (typeof web3 !== 'undefined') {
+            // If a web3 instance is already provided by Meta Mask.
+            App.web3Provider = web3.currentProvider;
+            web3 = new Web3(web3.currentProvider);
+        } else {
+            // Specify default instance if no web3 instance provided
+            App.web3Provider = new Web3.providers.HttpProvider('http://localhost:8545');
+            web3 = new Web3(App.web3Provider);
+        }
+        return App.initContract();
+    },
+
+    initContract: function () {
+        console.log("Function initContract")
+        App.contracts.Coinjob = contract(contractJson);
+        App.contracts.Coinjob.setProvider(App.web3Provider);
+        App.render();
+    },
+
+    render: function () {
+        console.log("Function render")
+
+        App._loading();
+        // Load account data
+        ethereum.request({ method: 'eth_requestAccounts' }).then(function (accounts) {
+            if (accounts) {
+                App.account = accounts[0];
+                $("#accountAddress").html("Your Account: " + App.account);
+            }
+            App._loaded();
+        });
+
+    },
+
+    allJob: function (page = -1, perPage = 10) {
+        console.log("Function allJob")
+        let CoinjobInstance;
+
+        App._loading();
+
+        // Load contract data
+        return App.contracts.Coinjob.deployed().then(function (instance) {
+            CoinjobInstance = instance;
+            if (page !== -1 || !perPage) {
+                return CoinjobInstance.getPaginatedSquares(page, perPage);
+            }
+            console.log(`page: ${page}, perPage: ${perPage} 모든 Job을 가져옵니다.`);
+            return CoinjobInstance.allJob();
+        }).then(function (jobList) {
+            console.log(jobList);
+            App._loaded();
+            return jobList;
+        }).catch(function (error) {
+            console.error(error.toString());
+        });
+    },
+
+    publishJob: function (title, contet, dontdisturb, deadline) {
+        console.log("Function publishJob")
+        let CoinjobInstance;
+
+        App._loading();
+
+        return App.contracts.Coinjob.deployed().then(async function (instance) {
+            CoinjobInstance = instance;
+            console.log(title, content, dontdisturb, deadline);
+            const account = (await web3.eth.getAccounts())[0];
+            return CoinjobInstance.publishJob(title, contet, dontdisturb, deadline,
+                {
+                    from: account,
+                    value: web3.utils.toWei("0.5", "ether")
+                }
+            );
+        }).then(function (job) {
+            console.log(job);
+            App._loaded();
+            return job;
+        }).catch(function (error) {
+            console.error(error);
+        });
+    },
+
+    acceptJob: function (_number, _contact) {
+
+    },
+
+    workDone: function (_number) {
+
+    },
+
+    finishJob: function (_number) {
+
+    },
+
+    giveupJob: function (_number) {
+
+    },
+
+
+    /**
+     * 프론트는 위의 함수들만 사용하세요.
+     */
+    _loading: function () {
+        let loader = $("#loader");
+        let content = $("#content");
+        loader.show();
+        content.hide();;
+    },
+
+    _loaded: function () {
+        let loader = $("#loader");
+        let content = $("#content");
+        loader.hide();
+        content.show();
+    },
+};
+
+export default App;
---
+++ test/testJobs.js
@@ -0,0 +1,66 @@
+const Coinjob = artifacts.require("./contracts/CoinJob.sol");
+const web = require("web3");
+https://github.com/truffle-box/pet-shop-box/blob/master/src/js/app.js
+contract('Coinjob', function (accounts) {
+    let coinjobInstance;
+
+    const title = "Hello";
+    const contet = "I want you.";
+    const dontdisturb = web3.utils.toWei("1", "ether");
+    const deadline = Date.now();
+
+    it("Job publish minimum cost 테스트 (0.5 ether 이상을 보상으로 걸어야함)", function () { // 무슨 minimum cost 테스트 할건지 : it
+        return Coinjob.deployed().then(function (instance) { // 배포가 됐다면
+            coinjobInstance = instance;
+
+            return coinjobInstance.publishJob(
+                title, contet, dontdisturb, deadline,
+                {
+                    from: accounts[0],
+                    value: web3.utils.toWei("0.1")
+
+                });
+        }).then(function () {
+            assert.fail("Job rewad는 0.5 이상이어야 합니다.");
+        }).catch(function (e) {
+            assert.equal(e, "AssertionError: Job rewad는 0.5 이상이어야 합니다.")
+        });
+    });
+
+    const publishCount = Math.ceil(Math.random() * 100 % 10);
+    const perPage = 10;
+
+    it("Job publish 테스트", function () { // 무슨 테스트 할건지 : it
+
+        return Coinjob.deployed().then(function (instance) { // 배포가 됐다면
+            coinjobInstance = instance;
+            for (let i = 1; i < publishCount; i++) {
+                const title = "Hello";
+                const contet = "I want you.";
+                const dontdisturb = web3.utils.toWei("1", "ether");
+                const deadline = Date.now();
+                coinjobInstance.publishJob(title, contet, dontdisturb, deadline,
+                    {
+                        from: accounts[0],
+                        value: web3.utils.toWei("0.5", "ether")
+
+                    });
+            }
+            return coinjobInstance.allJob();
+        }).then(function (jobList) {
+            assert.equal(jobList.length, publishCount);
+        });
+    });
+
+    it("Job pagination 테스트", function () { // 무슨 테스트 할건지 : it
+        return Coinjob.deployed().then(function (instance) { // 배포가 됐다면
+            coinjobInstance = instance;
+            return coinjobInstance.getPaginatedSquares(1, perPage);
+        }).then(function (jobList) {
+            assert.equal(jobList.length, perPage);
+            return coinjobInstance.getPaginatedSquares(2, perPage);
+        }).then(function (jobList) {
+            assert.equal(jobList.length, publishCount - perPage);
+        });
+    });
+});
